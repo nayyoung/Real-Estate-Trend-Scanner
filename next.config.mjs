facd006a7f068f813b483e318e006bf4d4c43c08
N. Joelle Young <nayayoung92@gmail.com>
2025-12-10 22:14:59 -0800

    Enable React strict mode and experimental features

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    serverActions: true,
+    turbo: true,
+  },
+};
 
 export default nextConfig;
